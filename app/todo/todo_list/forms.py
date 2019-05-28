bbb6636f3543882e672e6ff7d47cc27e43fb7f20
androidneha <neha.8xx@gmail.com>
2019-05-28 09:27:24 -0700

    some more method added for CRUD operation

---
+++ app/todo/todo_list/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import List
+
+class ListForm(forms.ModelForm):
+	class Meta:
+		model = List
+		fields = ['item', 'completed']
+			
+		
---
+++ app/todo/todo_list/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
+
+def home(request):
+	if request.method == 'POST':
+		form = ListForm(request.POST or None)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request, ('Item has been added to list.'))
+			return render(request, 'home.html', {'all_items': all_items})
+	else:
+		all_items = List.objects.all
+		return render(request, 'home.html', {'all_items': all_items})
+
+def delete(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.delete()
+	messages.success(request, ('Item has been deleted'))
+	return redirect('home')
+
+def cross_off(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = True
+	item.save()
+	return redirect('home')
+
+def uncross(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = False
+	item.save()
+	return redirect('home')
